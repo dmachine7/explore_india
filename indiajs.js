ddf8a95ae69f85791f2645888e3eb1418b514fe3
dmachine7 <60094449+dmachine7@users.noreply.github.com>
2020-05-19 13:41:16 +0530

    map and login added

--- indiajs.js
+++ indiajs.js
@@ -43,3 +43,11 @@ function displayon(i) {
 function displayoff(i) {
   document.getElementsByClassName("articlediv")[i].style.display = "none";
 }
+//included google map
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(20.5937,78.9629),
+  zoom:5,
+};
+var map = new google.maps.Map(document.getElementById("indiamap"),mapProp);
+}
